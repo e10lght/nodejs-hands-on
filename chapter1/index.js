cb9c2b536affd85a951d1e3e0bf1e9d77f743beb
kitaito <kira_neymar@icloud.com>
2022-11-11 03:56:28 +0900

    chapter1

--- chapter1/index.js
+++ chapter1/index.js
@@ -1,7 +1,9 @@
 /**
- * オブジェクトリテラル
- * スプレッド構文を利用し、オブジェクトのすべてのキーと値の組を渡す
+ * イミュータブル(元のオブジェクトを変更しない)に行う際は、
+ * スプレッド構文、レスト構文を利用する。
  **/ 
+
+/* ---スプレッド構文の例--- */
  const obj1 = {
     propA: 1,
     propB: 2
@@ -9,14 +11,14 @@
 const obj2 = { ...obj1, propC: 3 }
 console.log(obj2);  // { propA: 1, propB: 2, propC: 3 }
 
-/**
- * イミュータブル(元のオブジェクトを変更しない)に行う際は、
- * スプレッド構文、レスト構文を利用する。
- */
+/* ---レスト構文の例--- */
 const {propA, ...obj3} = obj2;
-// 元のオブジェクトは不変
-console.log(obj2)
-// 新しく生成されたオブジェクトはpropAが削除されている
-console.log(obj3)
-// レスト構文によりpropAは切り離される
-console.log(propA)
+
+/* 元のオブジェクトは不変 */
+console.log(obj2)  // { propA: 1, propB: 2, propC: 3 }
+
+/* 新しく生成されたオブジェクトはpropAが削除されている */
+console.log(obj3)  // { propB: 2, propC: 3 }
+
+/* レスト構文によりpropAは切り離される */
+console.log(propA)  // 1
